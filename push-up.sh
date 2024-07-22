789b332b689461c6134e8e803170fc2ff1039042
Andrew C. Scoppa <andrew-scoppa@github.com>
2024-07-22 12:00:22 -0400

    Update push-up.sh

--- push-up.sh
+++ push-up.sh
@@ -1,10 +1,10 @@
 # This file contains Git commands that stage, commit, and push changes to the 'main' branch of the remote repository.
 
 # Stage all changes
-git stage .
+git add .
 
 # Commit changes
-git commit -m "Update"
+git commit -m "Update the bash script"
 
 # Push changes to the 'main' branch of the remote repository
 git push origin main
